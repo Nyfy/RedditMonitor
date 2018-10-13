04b4c44240f52d48bad0aa658a276a3e198614ec
Marc Gagnon <nyfynexus@gmail.com>
2018-10-13 13:59:34 -0400

    finalized multi-subreddit support, implemented user PM alerts

--- src/main/java/monitor/MechmarketMonitor.java
+++ src/main/java/monitor/MechmarketMonitor.java
@@ -19,10 +19,12 @@ import net.dean.jraw.models.SubredditSort;
 import net.dean.jraw.oauth.Credentials;
 import net.dean.jraw.oauth.OAuthHelper;
 import net.dean.jraw.pagination.DefaultPaginator;
+import net.dean.jraw.references.InboxReference;
+import net.dean.jraw.references.OtherUserReference;
 import net.dean.jraw.references.SubredditReference;
 
 public class MechmarketMonitor {
-    private static String CONFIG = "config.properties";
+    private static String CONFIG = "resources/config.properties";
     
     private static String PLATFORM_PROPERTY = "username";
     private static String APPID_PROPERTY = "appId";
@@ -31,14 +33,18 @@ public class MechmarketMonitor {
     private static String PASSWORD_PROPERTY = "password";
     private static String CLIENTID_PROPERTY = "clientId";
     private static String CLIENTSECRET_PROPERTY = "clientSecret";
+    private static String ALERT_ACCOUNT = "alertAccount";
     
     private static String SUBREDDIT_BASE = "subreddit";
     private static String SEARCHSTRINGS_BASE = "searchStrings";
+    private static String[] SUFFIXES = {"A","B","C","D","E","F","G","H","I","J"};
     
     private static String username;
     private static String password;
     private static String clientId;
     private static String clientSecret;
+
+    private static String targetUser;
     
     private static String platform;
     private static String appId;
@@ -46,7 +52,6 @@ public class MechmarketMonitor {
     
     private Properties props;
     private RedditClient redditClient;
-    private SubredditReference mechmarket;
     
     private Map<SubredditReference,List<String>> searchStrings;
     
@@ -72,6 +77,7 @@ public class MechmarketMonitor {
         platform = props.getProperty(PLATFORM_PROPERTY);
         appId = props.getProperty(APPID_PROPERTY);
         version = props.getProperty(VERSION_PROPERTY);
+        targetUser = props.getProperty(ALERT_ACCOUNT);
     }
     
     private void initializeRedditClient() {
@@ -83,13 +89,14 @@ public class MechmarketMonitor {
     
     private void initializeSubredditSearches() {
         searchStrings = new HashMap<SubredditReference,List<String>>();
-        String[] suffixes = {"A","B","C","D","E","F","G","H","I","J"};
         
-        for (String suffix : suffixes) {
+        for (String suffix : SUFFIXES) {
             String subreddit = props.getProperty(SUBREDDIT_BASE+suffix);
             String[] searches = props.getProperty(SEARCHSTRINGS_BASE+suffix).split(";");
             
-            searchStrings.put(redditClient.subreddit(subreddit), new ArrayList<String>(Arrays.asList(searches)));
+            if (!subreddit.isEmpty()) {
+                searchStrings.put(redditClient.subreddit(subreddit), new ArrayList<String>(Arrays.asList(searches)));
+            }
         }
     }
     
@@ -97,30 +104,32 @@ public class MechmarketMonitor {
         List<String> permalinksVisited = new ArrayList<String>();
         
         while (true) {
-            if (permalinksVisited.size() > 5) {
+            while (permalinksVisited.size() > searchStrings.size() * 10) {
                 permalinksVisited.remove(0);
             }
             
-            DefaultPaginator<Submission> paginator = mechmarket.posts()
-                    .limit(5)
-                    .sorting(SubredditSort.NEW)
-                    .build();
-            
-            Iterator<Submission> submissions = paginator.next().iterator();
-            
-            while (submissions.hasNext()) {
-                Submission submission = submissions.next();
-                String permalink = "https://www.reddit.com"+submission.getPermalink();
-                 
-                if (!permalinksVisited.contains(permalink)) {
-                    permalinksVisited.add(permalink);
-                    
-                    String submissionTitle = submission.getTitle().toLowerCase();
-                    String submissionBody = submission.getSelfText().toLowerCase();
-                    
-                    for (String s : searchStrings) {
-                        if (submissionBody.contains(s) || submissionTitle.contains(s)) {
-                            alert(s, permalink);
+            for (SubredditReference subredditRef : searchStrings.keySet()) {
+                DefaultPaginator<Submission> paginator = subredditRef.posts()
+                        .limit(10)
+                        .sorting(SubredditSort.NEW)
+                        .build();
+                
+                Iterator<Submission> submissions = paginator.next().iterator();
+                
+                while (submissions.hasNext()) {
+                    Submission submission = submissions.next();
+                    String permalink = "https://www.reddit.com"+submission.getPermalink();
+                     
+                    if (!permalinksVisited.contains(permalink)) {
+                        permalinksVisited.add(permalink);
+                        
+                        String submissionTitle = submission.getTitle().toLowerCase();
+                        String submissionBody = submission.getSelfText().toLowerCase();
+                        
+                        for (String search : searchStrings.get(subredditRef)) {
+                            if (submissionBody.contains(search) || submissionTitle.contains(search)) {
+                                alert(search, permalink);
+                            }
                         }
                     }
                 }
@@ -130,6 +139,8 @@ public class MechmarketMonitor {
     }
     
     private void alert(String matchedString, String permalink) {
-        System.out.println("Found match for "+matchedString+" at "+permalink);
+        System.out.println(matchedString);
+        InboxReference inbox = redditClient.me().inbox();
+        inbox.compose(targetUser, "Alert for "+matchedString, "New match found for `"+matchedString+"` found at "+permalink);
     }
 }
